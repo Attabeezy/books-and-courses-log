c6dd854e9d5015f1ad07c4d14d66bf952d68d673
Benjamin Attabra <attabeezy@gmail.com>
2025-05-08 18:07:47 +0000

    Add files via upload

---
+++ Bro Code/Python/brocode2.py
@@ -0,0 +1,12 @@
+#name = 'Bro'
+#age = 21
+#attractive = True
+
+name, age, attractive = 'Bro', 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+SpongeBob = Patrick = Sandy = Squidward = 30
+print(SpongeBob, Patrick, Sandy, Squidward)
