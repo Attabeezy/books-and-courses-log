0cb9222991cd18283f8bc91740dcb03dd8e20226
Benjamin Attabra <attabeezy@gmail.com>
2025-05-22 22:38:52 +0000

    Add files via upload

---
+++ Bro Code/Python/brocode6.py
@@ -0,0 +1,15 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+#print(round(pi))       #rounding
+#print(math.ceil(pi))       #raise to highest whole
+#print(math.floor(pi))      #reduce to lowest whole
+#print(abs(pi))     #absolute value
+#print(pow(pi, 2))      #power of
+#print(math.sqrt(16))      #square root
+#print(max(x, y, z, pi))        #comparing to find maximum value
+#print(min(x, y, z))        #comparing to find minimum value
