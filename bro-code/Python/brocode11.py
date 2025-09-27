099c11802b4e13ca2565efed9c12bdfa7167fa78
Benjamin Attabra <attabeezy@gmail.com>
2025-09-27 12:04:16 +0000

    cloned and reogranized

---
+++ bro-code/Python/brocode11.py
@@ -0,0 +1,22 @@
+import time
+# for loop = a statement that will execute a block of code a limited number of times
+# while loop = unlimited
+# for loop = limited
+
+#for i in range(10):
+#    print(i)
+
+#for i in range(10):
+#    print(i+1)
+
+#for i in range(50,101, 10):
+#    print(i)
+
+#for i in "Bro Code":
+#    print(i)
+
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)
+print('Happy New Year!')
+
