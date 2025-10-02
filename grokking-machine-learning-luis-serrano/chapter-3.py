201352bd16255b7dfc8484644f884f8e3a9a9e60
Benjamin Attabra <attabeezy@gmail.com>
2025-10-02 12:13:42 +0000

    commit

---
+++ grokking-machine-learning-luis-serrano/chapter-3.py
@@ -0,0 +1,15 @@
+# LINEAR REGRESSION
+
+"""
+feature = properties of a data point used for making predictions
+
+labels = targets we indeed on predicting based on features
+
+prediction = the output of the model
+
+weights = a factor multiplied with a corresponding feature that aid in generating our prediiction
+
+bias = a constant that serves as our base for our prediction.
+"""
+
+## page [ 47 ] 69 / 513
