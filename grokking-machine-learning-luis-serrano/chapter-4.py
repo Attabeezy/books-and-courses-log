d2df95e950477e349068f1dfa6f634215465f244
Benjamin Attabra <attabeezy@gmail.com>
2025-11-20 09:26:37 +0000

    grokking ml

--- grokking-machine-learning-luis-serrano/chapter-4.py
+++ grokking-machine-learning-luis-serrano/chapter-4.py
@@ -1,22 +1,21 @@
-"""
-Summary
+"""Summary
 • When it comes to training models, many problems arise. Two problems that come up
-quite often are underfitting and overfitting.
-• Underfitting occurs when we use a very simple model to fit our dataset. Overfitting
+quite often are under fitting and over fitting.
+• Under fitting occurs when we use a very simple model to fit our dataset. Overfitting
 occurs when we use an overly complex model to fit our dataset.
-• An effective way to tell overfitting and underfitting apart is by using a testing dataset.
+• An effective way to tell over fitting and under fitting apart is by using a testing dataset.
 • To test a model, we split the data into two sets: a training set and a testing set. The
 training set is used to train the model, and the testing set is used to evaluate the model.
 • The golden rule of machine learning is to never use our testing data for training or
 making decisions in our models.
 • The validation set is another portion of our dataset that we use to make decisions about
 the hyperparameters in our model.
-• A model that underfits will perform poorly in the training set and in the validation set. A
-model that overfits will perform well in the training set but poorly in the validation set.
+• A model that under fits will perform poorly in the training set and in the validation set. A
+model that over fits will perform well in the training set but poorly in the validation set.
 A good model will perform well on both the training and the validation sets.
 • The model complexity graph is used to determine the correct complexity of a model, so
-that it doesn’t underfit or overfit.
-• Regularization is a very important technique to reduce overfitting in machine learning
+that it doesn’t under fit or over fit.
+• Regularization is a very important technique to reduce over fitting in machine learning
 models. It consists of adding a measure of complexity (regularization term) to the error
 function during the training process.
 • The L1 and L2 norms are the two most common measures of complexity used in
@@ -24,3 +23,39 @@ regularization.
 • Using the L1 norm leads to L1 regularization, or lasso regression. Using the L2 norm
 leads to L2 regularization, or ridge regression.
 """
+
+# Exercise 4.1
+# a. Model 3
+# b. Model 4 is under fitting the data because it does poorly on both training (1.9) and testing (2.3)
+# c. Model 1 is over fitting the data because it does well on training (0.1) and poorly on testing (1.8)
+
+# Exercise 4.2
+"""_summary_
+given dataset with X & y; model y_pred = 2x^2 -5x + 4,
+determine lasso regression error (using L1 norm) and ridge regression error (using L2 norm) given lambda = 0.1
+"""
+X = [1, 2, 3, 4, 5]
+Y = [2, 2.5, 6, 14.5, 34]
+    
+def model(x):
+    return (2 * (x ** 2)) - (5 * x) + 4
+
+#calculate lasso and ridge regression errors
+
+l1_norm = abs(2) + abs(-5) + abs(4)
+l2_norm = (2 ** 2) + ((-5) ** 2) + (4 ** 2)
+total_error = 0
+lambda_ = 0.1
+
+
+
+for i, x in enumerate(X):
+    y_pred = model(x)
+    error = abs(Y[i] - y_pred)
+    total_error += error
+
+lasso_error = total_error + (lambda_ * l1_norm)
+ridge_error = total_error + (lambda_ * l2_norm)
+
+#print("Lasso Regression Error:", lasso_error)
+#print("Ridge Regression Error:", ridge_error)
--- grokking-machine-learning-luis-serrano/chapter-5.py
+++ grokking-machine-learning-luis-serrano/chapter-5.py
@@ -1,35 +1,35 @@
 import random
 import numpy as np
 
-"""
-Summary
-• Classification is an important part of machine learning. It is similar to regression in that
-it consists of training an algorithm with labeled data and using it to make predictions on
-future (unlabeled) data. The difference from regression is that in classification, the
-predictions are categories, such as yes/no, spam/ham, and so on.
-• Perceptron classifiers work by assigning a weight to each of the features and a bias. The
-score of a data point is calculated as the sum of products of the weights and features, plus
-the bias. If the score is greater than or equal to zero, the classifier predicts a yes.
-Otherwise, it predicts a no.
-• For sentiment analysis, a perceptron consists of a score for each of the words in the
-dictionary, together with a bias. Happy words normally end up with a positive score, and
-sad words with a negative score. Neutral words such as the likely end up with a score close
-to zero.
-• The bias helps us decide if the empty sentence is happy or sad. If the bias is positive, then
-the empty sentence is happy, and if it is negative, then the empty sentence is sad.
-• Graphically, we can see a perceptron as a line trying to separate two classes of points,
-which can be seen as points of two different colors. In higher dimensions, a perceptron is
-a hyperplane separating points.
-• The perceptron algorithm works by starting with a random line and then slowly moving
-it to separate the points well. In every iteration, it picks a random point. If the point is
-correctly classified, the line doesn’t move. If it is misclassified, then the line moves a little
-bit closer to the point to pass over it and classify it correctly.
-• The perceptron algorithm has numerous applications, including spam email detection,
-recommendation systems, e-commerce, and health care.
-"""
 
-features = np.array([[1,0],[0,2],[1,1],[1,2],[1,3],[2,2],[2,3],[3,2]])
-labels = np.array([0,0,0,0,1,1,1,1])
+# Summary
+# • Classification is an important part of machine learning. It is similar to regression in that
+# it consists of training an algorithm with labeled data and using it to make predictions on
+# future (unlabeled) data. The difference from regression is that in classification, the
+# predictions are categories, such as yes/no, spam/ham, and so on.
+# • Perceptron classifiers work by assigning a weight to each of the features and a bias. The
+# score of a data point is calculated as the sum of products of the weights and features, plus
+# the bias. If the score is greater than or equal to zero, the classifier predicts a yes.
+# Otherwise, it predicts a no.
+# • For sentiment analysis, a perceptron consists of a score for each of the words in the
+# dictionary, together with a bias. Happy words normally end up with a positive score, and
+# sad words with a negative score. Neutral words such as the likely end up with a score close
+# to zero.
+# • The bias helps us decide if the empty sentence is happy or sad. If the bias is positive, then
+# the empty sentence is happy, and if it is negative, then the empty sentence is sad.
+# • Graphically, we can see a perceptron as a line trying to separate two classes of points,
+# which can be seen as points of two different colors. In higher dimensions, a perceptron is
+# a hyperplane separating points.
+# • The perceptron algorithm works by starting with a random line and then slowly moving
+# it to separate the points well. In every iteration, it picks a random point. If the point is
+# correctly classified, the line doesn’t move. If it is misclassified, then the line moves a little
+# bit closer to the point to pass over it and classify it correctly.
+# • The perceptron algorithm has numerous applications, including spam email detection,
+# recommendation systems, e-commerce, and health care.
+#
+#
+# features = np.array([[1,0],[0,2],[1,1],[1,2],[1,3],[2,2],[2,3],[3,2]])
+# labels = np.array([0,0,0,0,1,1,1,1])
 
 # THE PERCEPTRON ALGORITHM
 def score(weights, bias, features):
@@ -75,7 +75,7 @@ def perceptron_algorithm(features, labels, learning_rate = 0.01, epochs = 200):
         weights, bias = perceptron_trick(weights, bias, features[i], labels[i], learning_rate=learning_rate)
         return weights, bias, errors
 
-print(perceptron_algorithm(features, labels))
+# print(perceptron_algorithm(features, labels))
 
 
 # Exercise 5.1: Perceptron Implementation for given data
@@ -121,3 +121,81 @@ weights, bias, errors = perceptron_algorithm(df[['Cough', 'Fever', 'Difficulty b
 print("Weights:", weights)
 print("Bias:", bias)
 print("Errors:", errors)
+
+
+
+# Exercise 5.2
+
+# given the point (x1, x2) has the prediction y = step(2x1 + 3x2 -4),
+# and the model has boundary line 2x1 _ 3x2 -4 = 0,
+# we have p =(1,1) with label y=0
+
+# verify that p is misclassified
+
+def model(x1, x2):
+    return step((2 * x1) + (3 * x2) - 4)
+
+print("Prediction for point (1,1):", model(1, 1), "\nShould be 0")  # should be 0
+
+# yes, misclassified
+
+# b. calculate perceptron error that model produces at point p
+
+weights_np = np.array([2.0, 3.0])
+features_np = np.array([[1.0, 1.0]])  # shape (1, 2)
+labels_np = np.array([0])             # shape (1,)
+
+pError = mean_perceptron_error(weights_np, -4.0, features_np, labels_np)
+print("Perceptron Error at point (1,1):", pError)
+
+# c. apply perceptron trick to update the boundary line to still misclassify point p but reduce error, use learning rate 0.01
+
+
+new_weights, new_bias = perceptron_trick(weights_np, -4.0, features_np[0], labels_np[0], learning_rate=0.01)
+print("Updated weights:", new_weights)
+print("Updated bias:", new_bias)
+new_pError = mean_perceptron_error(new_weights, new_bias, features_np, labels_np)
+print("New Perceptron Error at point (1,1):", new_pError)
+
+# d. find new prediction given new model for point p
+new_prediction = prediction(new_weights, new_bias, features_np[0])
+print("New prediction for point (1,1):", new_prediction)
+new_pError = mean_perceptron_error(new_weights, new_bias, features_np, labels_np)
+print("New Perceptron Error at point (1,1):", new_pError)
+
+
+# Exercise 5.3: Using Perceptrons for building logical gates AND and OR
+x = np.array([[0,0],[0,1],[1,0],[1,1]])
+
+# a. AND gate
+y = np.array([0,0,0,1])  # AND gate labels
+
+def AND_gate():
+    weights, bias, errors = perceptron_algorithm(x, y, learning_rate=0.1, epochs=100)
+    return weights, bias, errors
+
+print("AND gate weights and bias:", AND_gate())
+
+# b. OR gate
+y = np.array([0,1,1,1])  # OR gate labels
+
+def OR_gate():
+    weights, bias, errors = perceptron_algorithm(x, y, learning_rate=0.1, epochs=100)
+    return weights, bias, errors
+
+print("OR gate weights and bias:", OR_gate())
+
+# c. Explain why a single perceptron cannot be used to build a XOR gate.
+"""_summary_
+A single perceptron cannot be used to build a XOR gate because the XOR function is not linearly separable.
+This means that there is no straight line (or hyperplane in higher dimensions) that can separate
+the input space into two regions corresponding to the two classes of the XOR function.
+As a result, a single-layer perceptron, which can only learn linear decision boundaries, cannot correctly classify the XOR inputs."""
+
+y = np.array([0,1,1,0])  # XOR gate labels
+
+def XOR_gate():
+    weights, bias, errors = perceptron_algorithm(x, y, learning_rate=0.1, epochs=100)
+    return weights, bias, errors
+
+print("XOR gate weights and bias (should give us a large error):", XOR_gate())
