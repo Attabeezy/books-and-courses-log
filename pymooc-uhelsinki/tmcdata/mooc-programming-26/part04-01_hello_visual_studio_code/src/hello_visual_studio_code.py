450fd5aac7a32acc0d653bac38f76c16531426b5
Benjamin Attabra <attabeezy@gmail.com>
2026-02-22 04:33:57 +0000

    Add compiled Python files for TMC exercise part04-01_hello_visual_studio_code
    - Generated __pycache__ files for the following modules:
      - __init__.py
      - __main__.py
      - points.py
      - result.py
      - runner.py
      - utils.py
    
    These files are necessary for the execution of the TMC tests and include compiled bytecode for improved performance.

--- pymooc-uhelsinki/tmcdata/mooc-programming-26/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ pymooc-uhelsinki/tmcdata/mooc-programming-26/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,2 +1,17 @@
 # Write your solution here
-print("Hello, VS Code")
+editors = {
+    "emacs" : "not good",
+    "vim" : "not good",
+    "word" : "awful",
+    "notepad" : "awful",
+    "atom" : "not good",
+    "visual studio code" : "an excellent choice!",
+}
+
+while True:
+    editor = input("Editor: ").lower()
+    if editor != "visual studio code":
+        print(editors[editor])
+    else:
+        print(editors[editor])
+        break
